f212db641a2ca32001b5de9701a07d3c8b732319
Martin Acosta <tinchoz49@gmail.com>
2020-07-30 11:07:25 -0300

    minor fix

--- src/round-robin-reader.test.js
+++ src/round-robin-reader.test.js
@@ -15,11 +15,29 @@ import createBatchStream from './create-batch-stream';
 import { FeedStore } from './feed-store';
 import FeedDescriptor from './feed-descriptor';
 
+const { Readable } = require('stream');
+
+class Counter extends Readable {
+  constructor (opt) {
+    super(opt);
+    this._max = 1000000;
+    this._index = 1;
+  }
+
+  _read () {
+    const i = this._index++;
+    if (i > this._max) { this.push(null); } else {
+      const str = String(i);
+      const buf = Buffer.from(str, 'ascii');
+      this.push(buf);
+    }
+  }
+}
+
 /**
  * Creates a multi ReadableStream for feed streams.
  */
 export default class OrderedReader {
-
   /** @type {(feedDescriptor, message) => Promise<boolean>} */
   _evaluator;
 
@@ -32,41 +50,57 @@ export default class OrderedReader {
   /** @type {() => void | undefined} */
   _wakeUpReader;
 
-  constructor(evaluator) {
+  _hasData;
+
+  constructor (evaluator) {
     this._evaluator = evaluator;
 
-    this._stream = from2.obj(async (size, next) => {
+    this._hasData = new Promise(resolve => {
+      this._wakeUpReader = () => {
+        this._wakeUpReader = null;
+        resolve();
+      };
+    });
 
+    this._stream = from2.obj(async (size, next) => {
       const tryReading = async () => {
         const toPush = [];
-        for(const feed of this._feeds.values()) {
-          if(toPush.length >= size) break
 
-          const msg = feed.message ?? feed.stream.read();
-          console.log('reading from feed', feed.descriptor.path, msg, feed.stream._readableState.flowing)
-          if(!msg) continue;
-          if(await this._evaluator(feed.descriptor, msg)) {
-            toPush.push(msg)
+        this._hasData = new Promise(resolve => {
+          this._wakeUpReader = () => {
+            this._wakeUpReader = null;
+            resolve();
+          };
+        });
+
+        for (const feed of this._feeds.values()) {
+          if (toPush.length >= size) break;
+
+          const msg = feed.stream.read();
+          console.log('reading from feed', feed.descriptor.path, msg, feed.stream._readableState.flowing, feed.stream.readable);
+          if (!msg) continue;
+          if (await this._evaluator(feed.descriptor, msg)) {
+            toPush.push(msg);
           } else {
-            feed.message = msg
+            feed.message = msg;
           }
         }
 
-        if(toPush.length > 0) {
-          next(null, toPush)
-          return true
+        if (toPush.length > 0) {
+          next(null, toPush);
+          return true;
         } else {
-          return false
+          return true;
         }
-      }
+      };
 
-      while(!await tryReading()) {
-        await new Promise(resolve => { this._wakeUpReader = resolve; });
+      while (await tryReading()) {
+        await this._hasData;
       }
     });
   }
 
-  get stream() { return this._stream }
+  get stream () { return this._stream; }
 
   async addInitialFeedStreams (descriptors) {
     for (const descriptor of descriptors) {
@@ -80,12 +114,12 @@ export default class OrderedReader {
    * @param {FeedDescriptor} descriptor
    */
   async addFeedStream (descriptor) {
-    const stream = createBatchStream(descriptor.feed, { live: true })
+    const stream = createBatchStream(descriptor.feed, { live: true });
 
     stream.on('readable', () => {
-      console.log('feed readable', descriptor.path)
-      this._wakeUpReader?.()
-    })
+      console.log('feed readable', descriptor.path);
+      this._wakeUpReader?.();
+    });
 
     this._feeds.add({ descriptor, stream });
   }
@@ -120,8 +154,6 @@ test('OrderedReader', async () => {
   const onSync = jest.fn();
   const messages = [];
 
-
-
   let feed2Msgs = 0;
   const stream = feedStore.createOrderedStream(
     async (feedDescriptor, message) => {
@@ -134,14 +166,13 @@ test('OrderedReader', async () => {
   );
   stream.on('readable', () => {
     let message;
-    while(message = stream.read()) {
+    while (message = stream.read()) {
       if (message.data.startsWith('feed2')) {
         feed2Msgs++;
       }
       messages.push(message);
     }
   });
-
 
   stream.on('sync', onSync);
   await new Promise(resolve => eos(stream, () => resolve()));
