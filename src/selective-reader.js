861dbfa61838466cf58288355bb57913fbd4d1db
Dmytro Maretskyi <maretskii@gmail.com>
2020-07-31 15:30:42 +0300

    Revert to while(true) iml

--- src/selective-reader.js
+++ src/selective-reader.js
@@ -39,45 +39,37 @@ export default class SelectiveReader extends Readable {
     this._reading = true;
     this._needsData = false;
 
-    this._pollFeeds();
-  }
+    while (true) {
+      this._hasData = new Promise(resolve => { this._wakeUpReader = resolve; });
 
-  async _pollFeeds () {
-    this._hasData = new Promise(resolve => { this._wakeUpReader = resolve; });
+      for (const feed of this._feeds.values()) {
+        if (feed.buffer.length === 0) {
+          const messages = feed.stream.read();
+          if (!messages) continue;
+          feed.buffer.push(...messages);
+        }
 
-    for (const feed of this._feeds.values()) {
-      if (feed.buffer.length === 0) {
-        const messages = feed.stream.read();
-        // console.log('reading from feed', feed.descriptor.path, messages, feed.stream._readableState.flowing, feed.stream.readable);
-        if (!messages) continue;
-        feed.buffer.push(...messages);
-      }
-
-      // console.log('processing', feed.descriptor.path)
-      let message;
-      while ((message = feed.buffer.shift())) {
-        if (await this._evaluator(feed.descriptor, message)) {
-          // console.log('approved')
-          process.nextTick(() => this._wakeUpReader());
-          this._needsData = false;
-          if (!this.push(message)) {
-            // console.log('read ended')
-            this._reading = false;
-            return;
+        let message;
+        while ((message = feed.buffer.shift())) {
+          if (await this._evaluator(feed.descriptor, message)) {
+            process.nextTick(() => this._wakeUpReader());
+            this._needsData = false;
+            if (!this.push(message)) {
+              this._reading = false;
+              return;
+            }
+          } else {
+            feed.buffer.unshift(message);
+            break;
           }
-        } else {
-          // console.log('unshift', feed.descriptor.path)
-          feed.buffer.unshift(message);
-          break;
         }
       }
-    }
 
-    if (this._needsData && Array.from(this._feeds.values()).some(x => x.buffer.length > 0)) {
-      setTimeout(() => this._pollFeeds(), 0);
-    } else {
+      await new Promise(resolve => setTimeout(resolve, 0)); // yield so that other tasks can be processed
+      if (this._needsData && Array.from(this._feeds.values()).some(x => x.buffer.length > 0)) {
+        continue;
+      }
       await this._hasData;
-      setTimeout(() => this._pollFeeds(), 0);
     }
   }
 
