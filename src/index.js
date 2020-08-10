a8490890e11e68cf403aaa7c810c6d4fbcd3361b
Dmytro Maretskyi <maretskii@gmail.com>
2020-08-10 17:03:27 +0300

    Export create batch stream

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // Copyright 2019 DXOS.org
 //
 
+import createBatchStream from './create-batch-stream';
 export * from './feed-store';
 export * from './feed-descriptor';
+export { createBatchStream };
